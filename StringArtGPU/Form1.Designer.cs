e68302f090a9507b218e7a225448ec6e45cfefc1
CorruptMem <xmgr.sys@gmail.com>
2025-06-17 13:04:53 +0200

    Projektdateien hinzufügen.

---
+++ StringArtGPU/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace StringArtGPU
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControlPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // glControlPanel
+            // 
+            this.glControlPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.glControlPanel.Location = new System.Drawing.Point(0, 0);
+            this.glControlPanel.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.glControlPanel.MinimumSize = new System.Drawing.Size(1000, 1000);
+            this.glControlPanel.Name = "glControlPanel";
+            this.glControlPanel.Size = new System.Drawing.Size(1000, 1000);
+            this.glControlPanel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1000, 1001);
+            this.Controls.Add(this.glControlPanel);
+            this.MaximumSize = new System.Drawing.Size(1016, 1040);
+            this.MinimumSize = new System.Drawing.Size(1016, 1040);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel glControlPanel;
+    }
+}
+
